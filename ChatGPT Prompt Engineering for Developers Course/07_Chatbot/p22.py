42f163558feb1eb631fcef1a28957c4b8cf776cc
Tracey Wong <treycwong@gmail.com>
2023-04-30 15:34:50 +0800

    Add files via upload

---
+++ ChatGPT Prompt Engineering for Developers Course/07_Chatbot/p22.py
@@ -0,0 +1,35 @@
+import openai
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) #read local .env file
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+    print(str(response.choices[0].message))
+    return response.choices[0].message["content"]
+
+messages =  [  
+{'role':'system', 'content':'You are friendly chatbot.'},
+{'role':'user', 'content':'Hi, my name is Tracey'},
+{'role':'assistant', 'content': "Hi Tracey! It's nice to meet you. \
+Is there anything I can help you with today?"},
+{'role':'user', 'content':'Yes, you can remind me, What is my name?'}  ]
+
+response = get_completion_from_messages(messages, temperature=1)
+print(response)
